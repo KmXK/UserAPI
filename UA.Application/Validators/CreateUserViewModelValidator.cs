698ca682fb2bf1f50fbe9710e6e5bc5dd5fac7ab
KmX <guydo.k@yandex.by>
2023-10-08 00:30:22 +0300

    Updated validation for creating user

--- UA.Application/Validators/CreateUserViewModelValidator.cs
+++ UA.Application/Validators/CreateUserViewModelValidator.cs
@@ -9,19 +9,22 @@ public class CreateUserViewModelValidator : AbstractValidator<CreateUserViewMode
     public CreateUserViewModelValidator(
         IUserService userService)
     {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+        
         RuleFor(x => x.Name)
-            .NotEmpty();
+            .NotEmpty().WithMessage("Name is required.");
+    
+        RuleFor(x => x.Age)
+            .NotEmpty().WithMessage("Age is required.")
+            .Must(a => a > 0).WithMessage("Age must be positive integer.");
+    
+        RuleFor(x => x.Roles)
+            .NotEmpty().WithMessage("User must have at least one role.");
         
         RuleFor(x => x.Email)
-            .NotEmpty()
+            .NotEmpty().WithMessage("Email is required.")
             .EmailAddress().WithMessage("Invalid email.")
             .MustAsync(async (email, _) => await userService.DoesUserWithEmailExist(email) == false)
             .WithMessage("User with such email already exists.");
-        
-        RuleFor(x => x.Age)
-            .Must(a => a > 0).WithMessage("Age must be positive integer.");
-        
-        RuleFor(x => x.Roles)
-            .NotEmpty().WithMessage("User must have at least one role.");
     }
 }
