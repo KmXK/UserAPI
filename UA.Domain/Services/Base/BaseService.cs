2becc1a1c639c54983ecc87bc6b5ec0e0ca1664c
KmX <guydo.k@yandex.by>
2023-10-07 18:28:05 +0300

    Create base classes for domain services

---
+++ UA.Domain/Services/Base/BaseService.cs
@@ -0,0 +1,13 @@
+﻿using UA.Data.Core.Interfaces;
+
+namespace UA.Domain.Services.Base;
+
+public abstract class BaseService
+{
+    protected IUnitOfWork UnitOfWork { get; }
+
+    protected BaseService(IUnitOfWork unitOfWork)
+    {
+        UnitOfWork = unitOfWork;
+    }
+}
---
+++ UA.Domain/Services/Base/BaseService`2.cs
@@ -0,0 +1,16 @@
+﻿using UA.Data.Core.Interfaces;
+using UA.Data.Models.Base;
+using UA.Data.Repositories.Interfaces;
+
+namespace UA.Domain.Services.Base;
+
+public abstract class BaseService<TId, TEntity> : BaseService
+    where TEntity : Entity<TId>
+    where TId : struct
+{
+    protected IKeyedRepository<TId, TEntity> WorkRepository => UnitOfWork.GetKeyedRepository<TId, TEntity>();
+
+    protected BaseService(IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+    }
+}
