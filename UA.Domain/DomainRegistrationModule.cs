7c3d2b41887ede3ea4ff9cd94f250df7b8b2193a
KmX <guydo.k@yandex.by>
2023-10-07 18:27:48 +0300

    Added autofac module for domain

---
+++ UA.Domain/DomainRegistrationModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using UA.Domain.Services;
+using UA.Domain.Services.Interfaces;
+
+namespace UA.Domain;
+
+public sealed class DomainRegistrationModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<UserService>().As<IUserService>();
+    }
+}
