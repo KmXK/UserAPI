b1d6da05862ea090553c6a372c78be0b78e62116
KmX <guydo.k@yandex.by>
2023-10-07 15:20:15 +0300

    Added db connection to Web project

--- UA.Web/Program.cs
+++ UA.Web/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using AppContext = UA.Data.AppContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("UA.Data.Migrations")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,5 +26,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+services.GetService<AppContext>()!.Database.EnsureCreated();
 
 app.Run();
