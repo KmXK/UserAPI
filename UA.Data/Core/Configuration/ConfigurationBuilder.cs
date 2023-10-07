038f00c0a90840957a91737e198f2fe683511955
KmX <guydo.k@yandex.by>
2023-10-07 17:48:32 +0300

    Created ConfigurationBuilder for simple configuration creation

---
+++ UA.Data/Core/Configuration/ConfigurationBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using UA.Data.Models.Base;
+
+namespace UA.Data.Core.Configuration;
+
+public static class ConfigurationBuilder
+{
+    public static Configuration<TEntity> Build<TEntity>(
+        params Expression<Func<TEntity, object>>[] includes
+    ) where TEntity : Entity
+    {
+        var configuration = new Configuration<TEntity>();
+
+        foreach (var include in includes)
+        {
+            configuration.Add(include);
+        }
+
+        return configuration;
+    }
+
+    public static Configuration<TEntity> Build<TEntity>(
+        IEnumerable<Expression<Func<TEntity, object>>> includes
+    ) where TEntity : Entity
+    {
+        return Build(includes.ToArray());
+    }
+}
