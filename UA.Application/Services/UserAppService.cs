aef1007a7012914029be9cac815ced535ca60871
KmX <guydo.k@yandex.by>
2023-10-07 20:15:33 +0300

    Added validation for creating user

--- UA.Application/Services/UserAppService.cs
+++ UA.Application/Services/UserAppService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using UA.Application.Services.Interfaces;
+using UA.Application.Validators.Interfaces;
 using UA.Application.ViewModels;
 using UA.Domain.Models;
 using UA.Domain.Services.Interfaces;
@@ -9,18 +10,23 @@ namespace UA.Application.Services;
 internal sealed class UserAppService : IUserAppService
 {
     private readonly IUserService _userService;
+    private readonly IValidator _validator;
     private readonly IMapper _mapper;
 
     public UserAppService(
         IUserService userService,
+        IValidator validator,
         IMapper mapper)
     {
         _userService = userService;
+        _validator = validator;
         _mapper = mapper;
     }
     
     public async Task<UserViewModel> Create(CreateUserViewModel viewModel)
     {
+        await _validator.Validate(viewModel);
+        
         var model = _mapper.Map<CreateUserModel>(viewModel);
 
         var user = await _userService.Create(model);
