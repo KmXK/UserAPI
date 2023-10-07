bc176ea28092b70c324a5019ce7116ca5c47ade5
KmX <guydo.k@yandex.by>
2023-10-07 18:28:28 +0300

    Implemented user creation on domain layer

--- UA.Domain/Services/UserService.cs
+++ UA.Domain/Services/UserService.cs
@@ -1,13 +1,31 @@
-﻿using UA.Data.Models;
+﻿using UA.Data.Core.Interfaces;
+using UA.Data.Models;
 using UA.Domain.Models;
+using UA.Domain.Services.Base;
 using UA.Domain.Services.Interfaces;
 
 namespace UA.Domain.Services;
 
-public sealed class UserService : IUserService
+public sealed class UserService : BaseService<Guid, User>, IUserService
 {
-    public Task<User> Create(CreateUserModel model)
+    public UserService(IUnitOfWork unitOfWork) : base(unitOfWork)
     {
-        throw new NotImplementedException();
+    }
+    
+    public async Task<User> Create(CreateUserModel model)
+    {
+        var user = new User
+        {
+            Age = model.Age,
+            Email = model.Email,
+            Name = model.Name,
+            Roles = model.Roles.Select(x => new Role { Id = x }).ToList()
+        };
+
+        await WorkRepository.AddAsync(user);
+
+        await UnitOfWork.SaveChangesAsync();
+
+        return user;
     }
 }
