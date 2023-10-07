c42eb568474fd3c30a0bd20f1e0eeee56290052a
KmX <guydo.k@yandex.by>
2023-10-07 16:36:17 +0300

    Added Automapper infrastructure

---
+++ UA.Application/AutoMapper/DomainProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using UA.Application.ViewModels;
+using UA.Data.Models;
+
+namespace UA.Application.AutoMapper;
+
+public sealed class DomainProfile : Profile
+{
+    public DomainProfile()
+    {
+        CreateMap<User, UserViewModel>();
+    }
+}
--- UA.Web/Program.cs
+++ UA.Web/Program.cs
@@ -2,6 +2,7 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using UA.Application;
+using UA.Application.AutoMapper;
 using AppContext = UA.Data.AppContext;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +20,11 @@ builder.Services.AddDbContext<AppContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         b => b.MigrationsAssembly("UA.Data.Migrations")));
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile<DomainProfile>();
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
