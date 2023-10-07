22d71e4562b51ade0b7647fde76b47918720ea9a
KmX <guydo.k@yandex.by>
2023-10-07 15:18:19 +0300

    Created AppContext

---
+++ UA.Data/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using UA.Data.Models;
+
+namespace UA.Data;
+
+public sealed class AppContext : DbContext
+{
+    public AppContext(DbContextOptions<AppContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Role> Roles { get; }
+    public DbSet<User> Users { get; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
