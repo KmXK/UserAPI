72cafb558a2c93cc2cf1279512bd479b8e1d8fde
KmX <guydo.k@yandex.by>
2023-10-07 18:33:31 +0300

    Added roles to database via migration

--- UA.Data/Configurations/RoleConfiguration.cs
+++ UA.Data/Configurations/RoleConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UA.Data.Enums;
 using UA.Data.Models;
 
 namespace UA.Data.Configurations;
@@ -14,5 +15,28 @@ public sealed class RoleConfiguration : IEntityTypeConfiguration<Role>
         
         builder.Property(x => x.Id).ValueGeneratedNever();
         builder.Property(x => x.Name).IsRequired();
+
+        builder.HasData(
+            new Role
+            {
+                Id = RoleEnum.User,
+                Name = "User"
+            },
+            new Role
+            {
+                Id = RoleEnum.Admin,
+                Name = "User"
+            },
+            new Role
+            {
+                Id = RoleEnum.SuperAdmin,
+                Name = "Super Admin"
+            },
+            new Role
+            {
+                Id = RoleEnum.Support,
+                Name = "Support"
+            }
+        );
     }
 }
