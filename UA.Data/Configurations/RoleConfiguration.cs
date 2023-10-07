c9e7027999e0a0063634b98bdd07878f88f4d7e8
KmX <guydo.k@yandex.by>
2023-10-07 15:18:10 +0300

    Created fluent API configurations for models

---
+++ UA.Data/Configurations/RoleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UA.Data.Models;
+
+namespace UA.Data.Configurations;
+
+public sealed class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("Role");
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Id).ValueGeneratedNever();
+        builder.Property(x => x.Name).IsRequired();
+
+        builder
+            .HasMany<User>()
+            .WithOne(u => u.Role)
+            .HasForeignKey(x => x.RoleId);
+    }
+}
---
+++ UA.Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UA.Data.Models;
+
+namespace UA.Data.Configurations;
+
+public sealed class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("User", t =>
+        {
+            t.HasCheckConstraint("CK_User_Age", "Age >= 0");
+        });
+        
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name).IsRequired();
+        builder.Property(x => x.Age).IsRequired();
+        builder.Property(x => x.Email).IsRequired();
+
+        builder
+            .HasOne(x => x.Role)
+            .WithMany();
+    }
+}
