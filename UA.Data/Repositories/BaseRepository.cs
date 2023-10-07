9c33aab9797a4618184f8781a00f51d60f5e7014
KmX <guydo.k@yandex.by>
2023-10-07 17:02:56 +0300

    Created BaseRepository

---
+++ UA.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UA.Data.Models.Base;
+
+namespace UA.Data.Repositories;
+
+public abstract class BaseRepository<TEntity> where TEntity : Entity
+{
+    private readonly AppContext _context;
+    
+    protected BaseRepository(AppContext context)
+    {
+        _context = context;
+        Set = _context.Set<TEntity>();
+    }
+    
+    protected DbSet<TEntity> Set { get; }
+
+    public async Task<bool> AddAsync(TEntity entity)
+    {
+        var result = await Set.AddAsync(entity);
+        
+        return result.State == EntityState.Added;
+    }
+    
+    public bool Update(TEntity entity)
+    {
+        Set.Update(entity);
+
+        return _context.Entry(entity).State == EntityState.Modified;
+    }
+    
+    public bool Delete(TEntity entity)
+    {
+        if (_context.Entry(entity).State == EntityState.Detached)
+            Set.Attach(entity);
+
+        Set.Remove(entity);
+
+        return _context.Entry(entity).State == EntityState.Deleted;
+    }
+}
